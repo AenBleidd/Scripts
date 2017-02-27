a0b345446477c02304a4d86b88e0591448e0bd8b
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2017-02-28 01:06:33 +0200

    Add CPULoadBalancer.py to prevent CPU overheat

---
+++ CPULoadBalancer.py
@@ -0,0 +1,64 @@
+import sys
+import subprocess
+
+from winreg import *
+
+CURRENT_POWERPLAN_PATH = "SYSTEM\\CurrentControlSet\\Control\\Power\\User\\Default\\PowerSchemes"
+ALL_POWERPLANS_PATH = "SYSTEM\\CurrentControlSet\\Control\\Power\\User\\PowerSchemes\\"
+MAX_CPULOAD_PATH_VAL1 = "54533251-82be-4824-96c1-47b60b740d00"
+MAX_CPULOAD_PATH_VAL2 = "bc5038f7-23e0-4960-96da-33abaf5935ec"
+
+def getValue():
+    if len(sys.argv) == 2:
+        try:
+            return int(sys.argv[1])
+        except BaseException:
+            return 0
+    else:
+        return 0
+
+def getCurrentPowerPlan():
+    try:
+        key = OpenKey(HKEY_LOCAL_MACHINE, CURRENT_POWERPLAN_PATH)
+        val = QueryValueEx(key, "ActivePowerScheme")
+        CloseKey(key)
+        return val[0]
+    except BaseException:
+        return ""
+
+def runProcess(command):
+	process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+	output, err = process.communicate()
+	retcode = process.returncode
+	return (retcode, output, err)
+
+def changeLoad(value):
+    try:
+        activePowerPlan = getCurrentPowerPlan()
+        if activePowerPlan == "":
+            return
+        keyPath = ALL_POWERPLANS_PATH + activePowerPlan + "\\" + MAX_CPULOAD_PATH_VAL1 + "\\" + MAX_CPULOAD_PATH_VAL2
+        key = OpenKey(HKEY_LOCAL_MACHINE, keyPath)
+        acValue = QueryValueEx(key, "ACSettingIndex")[0] + value
+        dcValue = QueryValueEx(key, "DCSettingIndex")[0] + value
+        CloseKey(key)
+
+        bChanged = False
+        if acValue > 0 and acValue <= 100:
+            runProcess(["powercfg", "/SETACVALUEINDEX", activePowerPlan, MAX_CPULOAD_PATH_VAL1, MAX_CPULOAD_PATH_VAL2, str(acValue)])
+            bChanged = True
+        if dcValue > 0 and dcValue <= 100:
+            runProcess(["powercfg", "/SETDCVALUEINDEX", activePowerPlan, MAX_CPULOAD_PATH_VAL1, MAX_CPULOAD_PATH_VAL2, str(dcValue)])
+            bChanged = True
+        if bChanged:
+            runProcess(["powercfg", "/S", activePowerPlan])
+    except WindowsError as e:
+        print(e.strerror)
+        return
+    return
+
+def main():
+    changeLoad(getValue())
+    return
+
+main()
