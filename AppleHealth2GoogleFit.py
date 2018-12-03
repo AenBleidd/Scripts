3c22d24732770e2927d20cc7db224394e327c218
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2018-12-03 10:34:54 +0200

    GetAvailableDataSources implementation
    Signed-off-by: Vitalii Koshura <lestat.de.lionkur@gmail.com>

--- AppleHealth2GoogleFit.py
+++ AppleHealth2GoogleFit.py
@@ -3,10 +3,12 @@
 # pip install --upgrade google-auth-oauthlib
 
 import google.oauth2.credentials
+import json
 import xml.etree.ElementTree as et
 import sys
 import zipfile
 
+from datetime import datetime
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -24,15 +26,30 @@ def getSettings(file):
             myvars[name.strip()] = var.strip()
     return myvars
 
+def getSource(record):
+    source = {}
+    if 'sourceName' in record.attrib:
+        source['sourceName'] = record.attrib['sourceName']
+    if 'sourceVersion' in record.attrib:
+        source['sourceVersion'] = record.attrib['sourceVersion']
+    if 'device' in record.attrib:
+        source['device'] = record.attrib['device']
+    return source
+
 def processInputData(xmlfile):
     xml = et.parse(xmlfile)
     root = xml.getroot()
     print(root)
     records = []
+    sources = []
     for record in root:
         if record.tag == 'Record':
             if record.attrib['type'] not in records:
                 records.append(record.attrib['type'])
+            # creationDate = datetime.strptime(record.attrib['creationDate'], '%Y-%m-%d %H:%M:%S %z')
+            source = getSource(record)
+            if source not in sources:
+                sources.append(source)
             # if record.attrib['type'] == 'HKQuantityTypeIdentifierHeartRate':
             #     print(record.tag, record.attrib)
             # if record.attrib['type'] == 'HKQuantityTypeIdentifierStepCount':
@@ -41,7 +58,9 @@ def processInputData(xmlfile):
     print('===============================================')
     for r in records:
         print(r)
-
+    print('===============================================')
+    for s in sources:
+        print(s)
 
 settingsFileName = getParams()
 settings = getSettings(settingsFileName)
@@ -61,6 +80,15 @@ credentials = flow.run_local_server(
     success_message='The auth flow is complete; you may close this window.',
     open_browser=True
     )
+fitness_service = build("fitness", "v1", credentials = credentials)
+# available_data_sources = json.loads(fitness_service.users().dataSources().list(userId="me").execute())
+available_data_sources = fitness_service.users().dataSources().list(userId="me").execute()
+for ds in available_data_sources['dataSource']:
+    if "device" in ds:
+        print(ds['dataStreamName'], '[', ds['device']['model'], ']')
+    else:
+        print(ds['dataStreamName'])
+# print(available_data_sources)
 
 zip = zipfile.ZipFile(settings['ArchivePath'], 'r')
 for name in zip.namelist():
